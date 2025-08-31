643eb2ed34326756d995b62cd87896932ce39d72
JuFer007 <juniorfernandozumaetagolac@gmail.com>
2025-08-30 19:02:41 -0500

    cambios user img

--- javascript/scriptNavbar.js
+++ javascript/scriptNavbar.js
@@ -7,16 +7,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const logoutBtn = document.getElementById("logoutBtn");
     const toastEl = document.getElementById("toastBienvenida");
 
-    const userNameLS = localStorage.getItem("username");
-    const userPicLS  = localStorage.getItem("profilePic");
-
     const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+    const userNameLS = localStorage.getItem("username") || "Usuario";
+    const userPicLS  = localStorage.getItem("profilePic") || "/recursos/default-avatar.png";
 
     if (isLoggedIn) {
+        //Navbar usuario
         if (loginNav) loginNav.classList.add("d-none");
         if (userNav) userNav.classList.remove("d-none");
 
-        if (navUserPic && userPicLS) navUserPic.src = userPicLS;
+        if (navUserPic) navUserPic.src = userPicLS;
 
         if (offcanvasLogin) {
             offcanvasLogin.setAttribute("href", "/html/userProfile.html");
@@ -28,11 +28,10 @@ document.addEventListener("DOMContentLoaded", () => {
         if (toastEl) {
             const toast = new bootstrap.Toast(toastEl);
             toast.show();
-            localStorage.removeItem("isLoggedIn");
         }
 
     } else {
-        //No logueado
+        //Navbar no logueado
         if (loginNav) loginNav.classList.remove("d-none");
         if (userNav) userNav.classList.add("d-none");
 
@@ -44,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if (carrito) carrito.classList.add("d-none");
     }
 
-    //Botón cerrar sesión
+    // Botón cerrar sesión
     if (logoutBtn) {
         logoutBtn.addEventListener("click", (e) => {
             e.preventDefault();
@@ -52,6 +51,7 @@ document.addEventListener("DOMContentLoaded", () => {
             localStorage.removeItem("profilePic");
             localStorage.removeItem("coverImg");
             localStorage.removeItem("userBio");
+            localStorage.removeItem("isLoggedIn");
             window.location.href = "/index.html";
         });
     }
