09b7c65ae7565c80ca55317c30a78dcd77c185df
Cristiaann19 <huamancruzcristianjesus@gmail.com>
2025-09-14 00:40:33 -0500

    cambios

--- javascript/scriptInicioSesion.js
+++ javascript/scriptInicioSesion.js
@@ -1,24 +1,36 @@
 const loginForm = document.getElementById("loginForm");
 
+const usuariosPre = [
+    {usario:'ComicForgeAdmin',email :'admin@comicforge.com', password : '12345'}
+]
+
 
 if (loginForm) {
     loginForm.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        const email = document.getElementById("exampleInputEmail1").value.trim();
-        const password = document.getElementById("exampleInputPassword1").value.trim();
+        const emailImmput = document.getElementById("exampleInputEmail1");
+        const passwordImmput = document.getElementById("exampleInputPassword1");
 
-        const validEmail = "admin@comicforge.com";
-        const validPass = "12345";
+        const email = emailImmput.value.trim();
+        const password = passwordImmput.value.trim();
 
-        if (email === validEmail && password === validPass) {
-            localStorage.setItem("isLoggedIn", "true");
-            localStorage.setItem("showToast", "true");
-            actualizarNavbar(true);
-            window.location.href = "/html/userProfile.html";
-        } else {
-            alert("Usuario o contraseña incorrectos");
+        if(!email || !password){
+            alert('Por favor completa todos los campos');
+        }else{
+            const usuarioValidos = usuariosPre.find(e => e.email === email && e.password === password);
+            if(usuarioValidos){
+                localStorage.setItem('isLoggedIn','true');
+                localStorage.setItem('showToast','true');
+                actualizarNavbar(true);
+                window.location.href = "/html/userProfile.html";
+            }else{
+                alert('Email o contraseña incorrecta');
+                emailImmput.value = "";
+                passwordImmput.value = "";
+            }
         }
+
     });
 }
 
@@ -37,5 +49,37 @@ if (btnCerrarSesion) {
         localStorage.removeItem("isLoggedIn");
         actualizarNavbar(false);
         window.location.href = "/index.html";
+    });
+}
+
+//PARA EL REGISTRO
+const RegistroForm = document.getElementById('RegistroForm');
+if(RegistroForm){
+    RegistroForm.addEventListener('submit', function(e){
+        e.preventDefault();
+        const username = document.getElementById('inputUser').value.trim();
+        const email = document.getElementById('InputEmail1').value.trim();
+        const password1 = document.getElementById('InputPassword1').value.trim();
+        const password2 = document.getElementById('InputPassword2').value.trim();
+        const usuarioValido = usuariosPre.find(e=> e.usario === username)
+
+
+        if (!username || !email || !password1 || !password2) {
+            alert('Por favor, completa todos los campos para registrarte.');
+        } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+            alert('Ingresa un correo valido ');
+        } else if (password1.length < 6){
+            alert('La contraseña debe tener al menos 6 caracteres.');
+        } else if (password1 !== password2){
+            alert('Las contraseñas no coinciden')
+        }else if (usuarioValido) {
+            alert('El nombre de usuario no se encuentra disponible. Escoja otro.');
+        }else{
+            localStorage.setItem('isLoggedIn','true');
+            localStorage.setItem('showToast','true');
+            actualizarNavbar(true);
+            window.location.href = "/html/userProfile.html";
+        }
+
     });
 }
