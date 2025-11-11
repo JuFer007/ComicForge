88a790e26a124ba9cefb97344ce736a2e477d4a9
JuFer007 <juniorfernandozumaetagolac@gmail.com>
2025-11-11 11:23:44 -0500

    soluciones

--- src/main/java/com/web/ComicForge/ComicForgeApplication.java
+++ src/main/java/com/web/ComicForge/ComicForgeApplication.java
@@ -1,10 +1,14 @@
 package com.web.ComicForge;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ComicForgeApplication {
 	public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
 		SpringApplication.run(ComicForgeApplication.class, args);
 	}
 }
